234e1c63b4330a7e65733b430df63e01c27e686e
luzhangjian <1414377646@qq.com>
2018-01-01 14:42:06 +0800

    profile的定义与调用

---
+++ src/main/java/com/luzj/highlight_spring4/ch2/profile/DemoBean.java
@@ -0,0 +1,20 @@
+package com.luzj.highlight_spring4.ch2.profile;
+
+/**
+ * Created by MyPC on 2018/1/1.
+ */
+public class DemoBean {
+    public String context;
+
+    public DemoBean(String context) {
+        this.context = context;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+}
