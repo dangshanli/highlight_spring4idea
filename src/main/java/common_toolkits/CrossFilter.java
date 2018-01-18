612e3bb85f2d4a020e156edc0d86179803dcbce2
HollowDante <1414377646@qq.com>
2018-01-18 11:45:14 +0800

    CrossFilter工具类，跨域访问过滤器

--- src/main/java/common_toolkits/CrossFilter.java
+++ src/main/java/common_toolkits/CrossFilter.java
@@ -1,8 +1,57 @@
 package common_toolkits;
 
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2018/1/17.
  * 解决网站跨域访问的问题
  */
-public class CrossFilter {
+@Component
+public class CrossFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    /**
+     * 方案一，设置response返回属性"Access-Control-Allow-Origin"
+     * @param request
+     * @param response
+     * @param chain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse rep = (HttpServletResponse) response;
+
+        //todo 设置返回头response Head属性
+        String origin = req.getHeader("Origin");
+        rep.setHeader("Access-Control-Allow-Origin",origin);
+        /*//直接设置成*也可以，但是获取origin可以设置可访问域名列表进行过滤
+        /*//*表示什么域名都可以访问了
+        rep.setHeader("Access-Control-Allow-Origin","*");*/
+
+        rep.setHeader("Access-Control-Allow-Method","POST,GET,OPTIONS,DELETE");
+        rep.setHeader("Access-Control-Max-Age","3600");//设置缓存时间，3600秒
+        rep.setHeader("Access-Control-Allow-Header","Origin, No-Cache, X-Requested-With, " +
+                "If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, " +
+                "X-E4M-With,userId,token");//设置返回属性列表，这里根据情况添加，不一定要这些
+        rep.setHeader("Access-Control-Allow-Credentials","true");
+        rep.setHeader("XDomainRequestAllowed","1");
+
+        chain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
