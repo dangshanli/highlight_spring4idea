2b02cbba7bcac12cb04aae0d48d5db8a1c941e14
HollowDante <1414377646@qq.com>
2018-01-18 17:21:23 +0800

    MD5加密工具类

---
+++ src/main/java/common_toolkits/MD5Util.java
@@ -0,0 +1,75 @@
+package common_toolkits;
+
+import sun.security.provider.MD5;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Administrator on 2018/1/18.
+ * MD5加密工具
+ */
+public class MD5Util {
+
+    private final static String[] hexDigits = {"0", "1", "2", "3", "4",
+            "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+
+    /**
+     * 进行MD5加密
+     *
+     * @param originStr
+     * @return
+     */
+    public static String encodeByMD5(String originStr) {
+        String hexResult = null;
+        if (originStr != null && !"".equals(originStr)) {
+            try {
+                //创建指定算法的信息类
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                byte[] resultBytes = md.digest(originStr.getBytes());
+                hexResult = byteArrayToHex(resultBytes).toUpperCase();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        return hexResult;
+    }
+
+
+    /**
+     * 字节数组转成16进制字符串
+     *
+     * @param b
+     * @return
+     */
+    private static String byteArrayToHex(byte[] b) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < b.length; i++) {
+            sb.append(byteToHex(b[i]));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 将byte转换成16进制表示
+     *
+     * @param b
+     * @return
+     */
+    private static String byteToHex(byte b) {
+        int n = b;//转成整数
+        if (n < 0)
+            n += 256;
+        int b1 = b / 16;//十位
+        int b2 = b % 16;//个位
+        return hexDigits[b1] + hexDigits[b2];
+    }
+
+    public static void main(String[] args) {
+        Byte b = new Byte("101");
+        int a = b;
+        System.out.println(b.byteValue());
+        System.out.println(a);
+        System.out.println(byteToHex(b));
+    }
+}
