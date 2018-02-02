dc72a8b47917d77212eea2af2b2bc78a64b4550b
HollowDante <1414377646@qq.com>
2018-02-02 10:33:26 +0800

    Thumbnails生成缩略图，over

--- src/main/java/common_toolkits/twent_useful_segments/JavaSegments2.java
+++ src/main/java/common_toolkits/twent_useful_segments/JavaSegments2.java
@@ -2,7 +2,9 @@ package common_toolkits.twent_useful_segments;
 
 import net.coobird.thumbnailator.Thumbnails;
 
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 /**
  * @author luzj
@@ -12,6 +14,11 @@ import java.io.IOException;
  */
 public class JavaSegments2 {
 
+    /**
+     * 创建固定大小的图 width:400 height:500
+     * @param from
+     * @param to
+     */
     public void createConstantPic(String from,String to){
         try {
             Thumbnails.of(from)
@@ -24,10 +31,58 @@ public class JavaSegments2 {
         }
     }
 
+    /**
+     * 常用的Thumbnail api,比例缩小，旋转，转格式
+     * @param from
+     * @param to
+     */
+    public void createScalePic(String from,String to){
+        try {
+            Thumbnails.of(from)
+                    .scale(0.25f)//todo 按比例缩小0.25左右
+                    .rotate(180)//旋转180度
+                    .outputFormat("png")//转换图片格式
+                    .toFile(to);//输出到文件
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            System.out.println("传输完结，查看目录["+to+"]");
+        }
+    }
+
+    /**
+     * 将缩略图写到输出流
+     * @param from 图片来源
+     * @param to 图片输出流来源
+     * @return
+     * @throws IOException
+     */
+    private OutputStream createOutStream(String from,String to) throws IOException {
+        OutputStream os = new FileOutputStream(to);
+        Thumbnails.of(from)
+                .scale(0.1)
+                .toOutputStream(os);
+        return os;
+    }
+
+    /**
+     * 将缩略图输出到BufferedImage
+     * @param from 图片来源
+     * @param to 图片出口
+     * @throws IOException
+     */
+    private void createBufferedImage(String from,String to) throws IOException {
+        BufferedImage bufferedImage = Thumbnails.of(from)
+                .scale(0.1f)
+                .asBufferedImage();
+        ImageIO.write(bufferedImage,"png",new File(to));
+    }
+
     public static void main(String[] args) {
         JavaSegments2 segments2 = new JavaSegments2();
-        String from = "F:\\壁纸\\13.jpg",to="F:\\xx.jpg";
-        segments2.createConstantPic(from,to);
+        String from = "F:\\壁纸\\13.jpg",to="F:\\yy.png";
+//        segments2.createConstantPic(from,to);
+        segments2.createScalePic(from,to);
     }
 
 }
